adc7d5f26d09e7eb9f7207f0d8b3ab371653fd1f
yagobonardi <yagobonardi@gmail.com>
2020-01-16 21:03:57 -0300

    melhoria

--- index.js
+++ index.js
@@ -2,16 +2,11 @@ function setLocationTime(){
     let date = new Date();
     let hour = date.getHours();
 
-    if (hour > 05 && hour < 16){
+    if (hour > 05 && hour <= 16){
         document.getElementById('location').innerHTML = 'Brazil/SP &#127750';
-    }
-
-    if (hour >= 18){
+    } else if (hour > 16 && hour < 18){
+        document.getElementById('location').innerHTML = 'Brazil/SP &#127751;';
+    } else {
         document.getElementById('location').innerHTML = 'Brazil/SP &#127747';
     }
-
-    if (hour >= 16 && hour < 18){
-        document.getElementById('location').innerHTML = 'Brazil/SP &#127751;';
-    }
-
 }
