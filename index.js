2ca149bdd176ede2758503238a0c18898f828d4c
Yago Bonardi <30670318+yagobonardi@users.noreply.github.com>
2020-03-06 15:05:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,3 +14,28 @@ function setLocationTime(){
 function sendMail(){
     window.open('mailto:yagobonardi@gmail.com');
 }
+
+function changeTheme() {
+    var actualTheme = document.getElementById('content').className;
+
+    if (actualTheme === 'body-dark'){
+        document.getElementById('content').className = 'body-light';
+        document.getElementById('git').className = 'fab fa-github-alt icon-light';
+        document.getElementById('linkedin').className = 'fab fa-linkedin-in icon-light';
+        document.getElementById('twitter').className = 'fab fa-twitter icon-light';
+        document.getElementById('mail').className = 'fas fa-at icon-light';
+        document.getElementById('name').className = 'text-light';
+        document.getElementById('profession').className = 'text-light text-small';
+        document.getElementById('location').className = 'text-light text-small';
+    } 
+    else if (actualTheme === 'body-light') {
+        document.getElementById('content').className = 'body-dark';
+        document.getElementById('git').className = 'fab fa-github-alt icon-dark';
+        document.getElementById('linkedin').className = 'fab fa-linkedin-in icon-dark';
+        document.getElementById('twitter').className = 'fab fa-twitter icon-dark';
+        document.getElementById('mail').className = 'fas fa-at icon-dark';
+        document.getElementById('name').className = 'text-dark';
+        document.getElementById('profession').className = 'text-dark text-small';
+        document.getElementById('location').className = 'text-dark text-small';
+    }
+}
